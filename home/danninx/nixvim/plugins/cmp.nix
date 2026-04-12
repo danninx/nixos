365bcae706bf7a27d0396fff533f68ae6aab50ce
danninx <mail@dfajardo.dev>
2026-04-12 16:05:22 -0500

    fix cmp nested settings

--- home/danninx/nixvim/plugins/cmp.nix
+++ home/danninx/nixvim/plugins/cmp.nix
@@ -3,33 +3,32 @@
     blink-cmp = {
       enable = true;
       settings = {
-        settings = {
-          appearance = {
-            nerd_font_variant = "mono";
-            use_nvim_cmp_as_default = true;
-          };
-
-          completion = {
-            accept = {
-              auto_brackets = {
-                enabled = true;
-                semantic_token_resolution.enable = true;
-              };
-            };
-            documentation.auto_show = true;
-          };
-
-          keymap = {
-            preset = "enter";
-          };
-
-          fuzzy.implementation = "prefer_rust_with_warning";
-          signature.enabled = true;
-          sources.default = ["lsp" "path" "snippets" "buffer"];
+        appearance = {
+          nerd_font_variant = "mono";
         };
-      };
 
-      friendly-snippets.enable = true;
+        completion = {
+          accept = {
+            auto_brackets = {
+              enabled = true;
+              semantic_token_resolution.enable = true;
+            };
+          };
+          list.selection.preselect = true;
+          list.selection.auto_insert = true;
+          documentation.auto_show = true;
+        };
+
+        keymap = {
+          preset = "enter";
+        };
+
+        fuzzy.implementation = "prefer_rust_with_warning";
+        signature.enabled = true;
+        sources.default = ["lsp" "path" "snippets" "buffer"];
+      };
     };
+
+    friendly-snippets.enable = true;
   };
 }
