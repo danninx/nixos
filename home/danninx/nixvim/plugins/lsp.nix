9f8f01d73613f4000c872ea313beb88cc73e2d01
danninx <mail@dfajardo.dev>
2026-04-15 19:51:59 -0500

    add clangd, golang lsp config

--- home/danninx/nixvim/plugins/lsp.nix
+++ home/danninx/nixvim/plugins/lsp.nix
@@ -3,6 +3,45 @@
     enable = true;
     inlayHints = true;
     servers = {
+      golangci_lint_ls.enable = true;
+      gopls = {
+        enable = true;
+        settings = {
+          gopls = {
+            gofumpt = true;
+            codelenses = {
+              gc_details = false;
+              generate = true;
+              regenerate_cgo = true;
+              run_govulncheck = true;
+              test = true;
+              tidy = true;
+              upgrade_dependency = true;
+              vendor = true;
+            };
+            hints = {
+              assignVariableTypes = false;
+              compositeLiteralFields = true;
+              compositeLiteralTypes = true;
+              constantValues = true;
+              functionTypeParameters = true;
+              parameterNames = true;
+              rangeVariableTypes = true;
+            };
+            analyses = {
+              nilness = true;
+              unusedparams = true;
+              unusedwrite = true;
+              useany = true;
+            };
+            usePlaceholders = true;
+            completeUnimported = true;
+            staticcheck = true;
+            directoryFilters = [ "-.git" "-.vscode" "-.idea" "-.vscode-test" "-node_modules" ];
+            semanticTokens = true;
+          };
+        };
+      };
 
       nil_ls = {
         enable = true;
@@ -17,6 +56,10 @@
             };
           };
         };
+      };
+
+      clangd = {
+        enable = true;
       };
 
       qmlls.enable = true;
