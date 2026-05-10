3b00074875f978bd5cf6c8da5948147a8c509460
danninx <mail@dfajardo.dev>
2026-05-10 13:47:01 -0500

    add zed settings

--- home/danninx/zed.nix
+++ home/danninx/zed.nix
@@ -37,7 +37,25 @@
         dark = "Tokyo Night";
       };
 
-      direnv = "shell_hook";
+      tabs = {
+        file_icons = true;
+        git_status = false;
+      };
+
+      tab_bar = {
+        show_pinned_tabs_in_separate_row = false;
+        show_nav_history_buttons = false;
+        show_tab_bar_buttons = false;
+        show = true;
+      };
+
+      title_bar = {
+        show_onboarding_banner = false;
+        show_user_picture = false;
+        show_user_menu = false;
+        show_sign_in = false;
+        show_project_items = true;
+      };
     };
   };
 }
